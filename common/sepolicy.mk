ShapeShiftOS/android_device_ssos_sepolicy

Remove lineage trust sepolicy
Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

common/sepolicy.mk
@@ -23,8 +23,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/ssos/sepolicy/common/dynamic \
-    device/ssos/sepolicy/common/system
+    device/ssos/sepolicy/common/dynamic
 else
 BOARD_SEPOLICY_DIRS += \
     device/ssos/sepolicy/common/dynamic \

sherifrahim/device_nezuko_sepolicy

common/sepolicy.mk
@@ -23,8 +23,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/fluid/sepolicy/common/dynamic \
-    device/fluid/sepolicy/common/system
+    device/fluid/sepolicy/common/dynamic
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/fluid/sepolicy/common/dynamic \